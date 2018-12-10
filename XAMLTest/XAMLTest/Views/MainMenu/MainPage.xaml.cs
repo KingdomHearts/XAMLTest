b5517ce08aec3781d53fda15e898725bbe4f352a
Elias <eliasdragon55@hotmail.com>
2018-12-10 12:13:49 +0100

    Commit after merge to fully use the application

--- XAMLTest/XAMLTest/Views/MainMenu/MainPage.xaml.cs
+++ XAMLTest/XAMLTest/Views/MainMenu/MainPage.xaml.cs
@@ -5,7 +5,7 @@ using System.Text;
 using System.Threading.Tasks;
 using Xamarin.Forms;
 
-namespace XAMLTest
+namespace XAMLTest.Views.MainMenu
 {
     public partial class MainPage : ContentPage
     {
